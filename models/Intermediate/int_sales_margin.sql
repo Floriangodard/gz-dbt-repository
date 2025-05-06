1584f11dc79a171070ee3c09ffb4059f07964cfb
floriangodard92@gmail.com <floriangodard92@gmail.com>
2025-05-06 13:42:05 +0000

    regul

--- models/Intermediate/int_sales_margin.sql
+++ models/Intermediate/int_sales_margin.sql
@@ -1,4 +1,6 @@
-select*
- from {{ ref('stg_gz_raw_data__raw_gz_sales') }} 
-left join {{ ref('stg_gz_raw_data__raw_gz_product') }} 
-on gsa.pdt_id = gpr.product_id 
+select *,
+purchase_price*quantity as purchase_cost,
+revenue-purchase_price*quantity as margin
+ from {{ ref('stg_gz_raw_data__raw_gz_sales') }} as sales
+left join {{ ref('stg_gz_raw_data__raw_gz_product') }} as product
+on sales.product_id = product.products_id 
