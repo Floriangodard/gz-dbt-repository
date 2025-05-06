5ac592f7ea2c1be79af632a5627a09d5895c6d1c
floriangodard92@gmail.com <floriangodard92@gmail.com>
2025-05-06 14:59:14 +0000

    hjbhi

--- models/Intermediate/int_orders_operational.sql
+++ models/Intermediate/int_orders_operational.sql
@@ -1,4 +1,4 @@
-select *, revenue,
+select *, 
 margin+shipping_fee-logcost-ship_cost as operational_margin
 from {{ ref('int_orders_margin') }} as ordm 
 left join {{ ref('stg_gz_raw_data__raw_gz_ship') }} as gzship 
